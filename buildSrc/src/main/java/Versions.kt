c470a42e278438dee8128d42332c1b74b3e8aa7d
조준장 <wnswkd486@gmail.com>
2025-10-12 20:13:08 +0900

    chore: v3.0.4 version upgrade

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val COMPILE_SDK = 35
     const val MIN_SDK = 24
-    const val VERSION_CODE = 9
-    const val VERSION_NAME = "3.0.3"
+    const val VERSION_CODE = 10
+    const val VERSION_NAME = "3.0.4"
 }
