38c39b1d2fd20ab7962d18779daff13c8c65dc85
조준장 <wnswkd486@gmail.com>
2026-01-17 19:50:34 +0900

    chore: v3.1.1 update

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val COMPILE_SDK = 35
     const val MIN_SDK = 24
-    const val VERSION_CODE = 11
-    const val VERSION_NAME = "3.1.0"
+    const val VERSION_CODE = 12
+    const val VERSION_NAME = "3.1.1"
 }
