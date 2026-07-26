9940b2d9762521b55291e8e98c6a8813853a1b86
JunJangE <wnswkd486@gmail.com>
2026-07-26 13:48:53 +0900

    feat: qr floatingActionButton color 변경

--- feature/home/src/commonMain/kotlin/junjange/feature/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/junjange/feature/home/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -70,6 +71,8 @@ fun HomeScreen(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = navigateToQRScanner,
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
             ) {
                 Icon(
                     painter = painterResource(Res.drawable.ic_qr_code),
